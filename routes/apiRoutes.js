ed7f7f633da2e8b526f040af347c88b90577f430
Joy <joyumoekpo1@gmail.com>
2021-04-12 07:09:43 -0500

    Set up routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    console.log(body)
+    Workout.findByIdAndUpdate(params.id,
+        { $push: { exercises: body } }, { new: true })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .limit(10)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.delete("/api/workouts", ({ body }, res) => {
+    Workout.findByIdAndRemove(body.id)
+        .then(() => {
+            res.json(true);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
