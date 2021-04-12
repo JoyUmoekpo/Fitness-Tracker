1d4172e673fc270a7a8e745a7a5ba79705e0c235
Joy <joyumoekpo1@gmail.com>
2021-04-12 06:36:11 -0500

    Added routes to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const apiRoutes = require("./routes/apiRoutes.js");
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,13 +14,14 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Fitness-Tracker", {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
     useFindAndModify: false,
 });
 
+app.use(apiRoutes);
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
